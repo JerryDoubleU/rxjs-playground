a3ea9478b528ca1c3bf287bc1ce378a0be88fb88
Jurek Wozniak <jerzy@enplug.com>
2021-10-17 13:24:24 +0200

    initial release

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+
+import { Observable } from 'rxjs';
+
+const someObservable$ = new Observable<string>(subscriber => {
+  subscriber.next('Alice');
+  subscriber.next('Ben');
+  subscriber.next('Charlie');
+  subscriber.complete();
+});
+
+someObservable$.subscribe(value => console.log(value));
+
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    publicPath: '/',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  }
+};
